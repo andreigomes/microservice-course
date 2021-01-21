ec4c6ae76bf99879a0fef8f568e69b6152073512
Andrei Gomes <andrei_sps@hotmail.com>
2021-01-21 01:43:11 -0300

    feignClient

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,25 +1,31 @@
 package com.devsuperior.hrpayroll.services;
 
+import com.devsuperior.hrpayroll.WorkerFeignClient;
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        String url = workerHost + "/workers/" + workerId;
 
-        Worker worker = restTemplate.getForObject(url, Worker.class);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
+
+    //Com restTemplate
+//    public Payment getPayment(long workerId, int days) {
+//        String url = workerHost + "/workers/" + workerId;
+//
+//        Worker worker = restTemplate.getForObject(url, Worker.class);
+//        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+//    }
 }
